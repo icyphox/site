1a94e14dd5273d73f259b2ad8f77e5f9e7c8884c
Anirudh <icyph0x@pm.me>
2018-06-16 16:08:59 +0530

    chore: init

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py - Vite's configuration script
+
+title = 'icyphox'
+author = ''
+header = '<a href="/">home</a> <a href="/gpg.txt">gpg</a>'
+footer = '<p align="center">Built using <a href="https://github.com/icyphox/vite">Vite</a>.</p>'
+template = 'index.html'  # default is index.html
+               
