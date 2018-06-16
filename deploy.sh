eaf865e5ae511396f1c74a4a5208d2e3a5557668
Anirudh <icyph0x@pm.me>
2018-06-16 16:38:55 +0530

    refactor(deploy): hardcode `build` dir

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ]
-then
-	echo "no folder mentioned :v"
-	exit 1
-fi
-git subtree push --prefix $1 origin gh-pages
+echo "[*] Deploying to gh-pages..."
+git subtree push --prefix build origin gh-pages
