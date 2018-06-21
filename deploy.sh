c21b681c86cfa3dd6a6fb0cbc493a5ec23682643
Anirudh <icyph0x@pm.me>
2018-06-21 13:56:00 +0530

    style(deploy): colors!

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-echo "[*] Deploying to gh-pages..."
+# colors
+ylw='\033[0;33m'
+grn='\033[0;32m'
+rst='\033[0m'
+
+echo -e "$ylw[~]$rst Deploying to gh-pages..."
 git subtree push --prefix build origin gh-pages
-echo "[*] Done! Check it out at: https://icy.ph0x.me"
+echo -e "$grn[+]$rst Done! Check it out at: https://icy.ph0x.me"
