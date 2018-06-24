a9ecfd7df0a82be15cd8cb418cffbeec5225848a
Anirudh <icyph0x@pm.me>
2018-06-24 22:32:07 +0530

    refactor: minor css updates

--- config.py
+++ config.py
@@ -3,5 +3,5 @@
 title = 'icyphox'
 author = ''
 header = '<a href="/">home</a> <a href="/gpg.txt">gpg</a> <a href="/about">about</a> <a href="/contact">contact</a>'
-footer = '<small align="center">Built using <a href="https://github.com/icyphox/vite">Vite</a>. View this site\'s <a href="https://github.com/icyphox/site">source</a>.</small>'
+footer = 'Built using <a href="https://github.com/icyphox/vite">Vite</a>. View this site\'s <a href="https://github.com/icyphox/site">source</a>.'
 template = 'index.html'  # default is index.html
