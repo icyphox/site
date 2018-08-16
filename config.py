a7b2c9f3a73fe6a12cf9b16c97ec0a1a915c0581
Anirudh <icyph0x@pm.me>
2018-08-17 01:27:32 +0530

    refactor: a lot of things

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ title = 'Anirudh Oppiliappan'
 author = ''
 # header = '<a href="/">home</a> <a href="/projects">projects</a> <a href="/info">info</a> <a href="/gpg.txt">gpg</a>'
 header = ''
-footer = ''
+footer = '&copy; 2018 — <a href="mailto:icyph0x@pm.me">icyph0x@pm.me</a>'
 template = 'index.html'  # default is index.html
+
