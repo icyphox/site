17ac23fb0f8e8ccfeff85738d801b84edfe81b14
Anirudh <icyph0x@pm.me>
2018-09-05 15:52:10 +0530

    fix(header): links don't open in a new tab anymore
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 title = 'Anirudh Oppiliappan'
 author = ''
-header = '<a href="/" target="_blank">Home</a> <a href="/blog" target="_blank">Blog</a> <a href="/about" target="_blank">About</a>' 
+header = '<a href="/">Home</a> <a href="/blog">Blog</a> <a href="/about">About</a>' 
 footer = '&copy; 2018 — <a href="mailto:icyph0x@pm.me">icyph0x@pm.me</a>'
 template = 'index.html'  # default is index.html
 
