0aa83817bbc1948ec2fb8a6d47f16d86c0e9768b
Anirudh <icyph0x@pm.me>
2018-09-24 10:19:20 +0530

    refactor: remove any occurences of surname
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py - Vite's configuration script
 
-title = 'Anirudh Oppiliappan'
+title = 'Anirudh'
 author = ''
 header = '<a href="/">Home</a> <a href="/blog">Blog</a> <a href="/about">About</a>' 
 footer = '&copy; 2018 — <a href="mailto:icyph0x@pm.me">icyph0x@pm.me</a>'
