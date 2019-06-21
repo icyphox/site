b29ab6388b0b78f485bff5360b2f6d8a2c4e236f
Anirudh <icyph0x@pm.me>
2019-06-21 19:40:18 +0530

    Remove `vite build` from deploy script
    What's the point in building, if you're pushing w/o commiting?
    
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# build, because i never do
-vite build
-
 git push origin master
 # push contents of `build/` to the `gh-pages` branch
 git subtree push --prefix build origin gh-pages
