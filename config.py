e1232b9634a163da77f431dae038049125c19603
Anirudh <icyph0x@pm.me>
2019-06-24 15:29:16 +0530

    Better footer, end of article text
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- config.py
+++ config.py
@@ -3,6 +3,5 @@
 title = 'Anirudh'
 author = ''
 header = '<a href="/">icyphox.sh</a> (<a href="https://github.com/icyphox/site">src</a>)'
-footer = '' 
+footer = """<img src="https://licensebuttons.net/p/zero/1.0/80x15.png">"""
 template = 'index.html'  # default is index.html
-               
