a0a0b62a96ee33b66ee0e130735f02647293fbe3
Anirudh <icyph0x@pm.me>
2019-08-06 19:33:54 +0530

    Create RSS feed
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- rss.py
+++ rss.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
+# generate an rss item
 
 import html
 from markdown2 import markdown_path
 import sys
+import datetime
 import os
 
-mdfile = sys.argv[1]
+try:
+    mdfile = sys.argv[1]
+except IndexError:
+    print('error: specify path to markdown file')
+    sys.exit(-1)
 url =  os.path.splitext(mdfile)[0]
-rendered = markdown_path(os.path.join('pages/blog/', mdfile), extras=['metadata', 
+rendered = markdown_path(mdfile, extras=['metadata',
         'fenced-code-blocks', 'header-ids', 'footnotes', 'smarty-pants'])
 meta = rendered.metadata
 esc = html.escape(rendered)
 
+# time stuff
+dt = datetime.datetime.strptime(meta['date'], '%Y-%m-%d').date()
+rfc822 = dt.strftime("%a, %d %b %Y %H:%M:%S %z") + '+0000'
+
 item = f"""<item>
       <title>{meta['title']}</title>
+      <link>https://icyphox.sh/blog/{url}</link>
       <description>{esc}</description>
-      <guid isPermaLink="false">https://icyphox.sh/blog/{url}/</guid>
+      <pubDate>{rfc822}</pubDate>
+      <guid>https://icyphox.sh/blog/{url}/</guid>
 </item>
 """
 
