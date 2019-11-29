6a9940fd36950706276ab34aa607b85a7c75ba41
Anirudh Oppiliappan <x@icyphox.sh>
2019-11-29 21:52:01 +0530

    Remove twitter from header
    Signed-off-by: Anirudh Oppiliappan <x@icyphox.sh>

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ header = """
         <a href="/">home</a>
         <a href="/blog">blog</a>
         <a href="/reading">reading</a>
-        <a href="https://twitter.com/icyphox">twitter</a>
         <a href="/about">about</a>
 """
 footer = """<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
