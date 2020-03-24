131fc63b040d325f1555d49f49e464908f112479
Anirudh Oppiliappan <x@icyphox.sh>
2020-03-24 16:26:26 +0530

    New site design!
    Signed-off-by: Anirudh Oppiliappan <x@icyphox.sh>

--- config.py
+++ config.py
@@ -1,21 +1,59 @@
 # config.py - Vite's configuration script
 
-title = 'icyphox'
-author = ''
-header = """
-    <div class="navbar">
-        <a href="/">home</a>
-        <a href="/blog">blog</a>
-        <a href="/friends">friends</a>
-        <a href="/about">about</a>
+title = "icyphox"
+author = ""
+header = """<a href="/"><- back</a>"""
+
+# actually the sidebar
+footer = """
+    <img class="logo" src="/static/icyphox.png" alt="icyphox's avatar" />
+    <p class="muted">
+    Memeing security since forever.
+    </p>
+
+    <p>
+    <span class="sidebar-link">email</span>
+    <br>
+      <a href="mailto:x@icyphox.sh">x@icyphox.sh</a>
+    </p> 
+
+    <p>
+    <span class="sidebar-link">github</span>
+        <br>
+      <a href="https://github.com/icyphox">icyphox</a>
+    </p> 
+
+    <p>
+    <span class="sidebar-link">twitter</span>
+    <br>
+      <a href="https://twitter.com/icyphox">icyphox</a>
+    </p> 
+
+
+    <p>
+    <span class="sidebar-link">pgp</span>
+    <br>
+      <a href="/static/gpg.txt">0x8A93F96F78C5D4C4</a>
+    </p> 
+
+    <h3>friends</h3>
+    <p>
+    Some of <a href="/friends">my friends</a> and internet bros.
+    </p>
+
+    <h3>about</h3>
+    <p>
+    More <a href="/about">about me</a> and my work.
+    </p>
+
+    <div class="license">
+    <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
+    <img class="footimgs" src="/static/cc.svg">
+    </a>
+    <a href="https://webring.xxiivv.com/#random" target="_blank">
+    <img class="footimgs" alt="xxiivv webring" src="/static/webring.svg">
+    </a>
     </div>
-"""
-footer = """<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
-        <img class="footimgs" src="/static/cc.svg">
-        </a>
-        <a href="https://webring.xxiivv.com/#random" target="_blank">
-        <img class="footimgs" alt="xxiivv webring" src="/static/webring.svg">
-        </a>
         """
 template = 'text.html'  # default is index.html
-post_build = ['./bin/rss.py', './bin/plaintext.sh', './bin/update_index.py']
+# post_build = ['./bin/rss.py', './bin/plaintext.sh', './bin/update_index.py']
