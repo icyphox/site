e57151bcca4945bbb86be0a5351151fbeed2f257
Anirudh Oppiliappan <x@icyphox.sh>
2020-06-13 17:51:03 +0530

    Create bin/commit.py
    Adds the latest commit link to the sidebar. How useful is this? I dunno,
    but it's cool.
    
    Signed-off-by: Anirudh Oppiliappan <x@icyphox.sh>

--- config.py
+++ config.py
@@ -31,6 +31,12 @@ footer = """
       <a href="/static/gpg.txt">0x8A93F96F78C5D4C4</a>
     </p> 
 
+    <p>
+    <span class="sidebar-link">last updated</span>
+    <br>
+    <a href="https://github.com/icyphox/site/commit/{{ big_commit }}">{{ commit }}</a> on {{ commit_date }}
+    </p>
+
     <h3>friends</h3>
     <p>
     Some of <a href="/friends">my friends</a> and internet bros.
@@ -41,18 +47,16 @@ footer = """
     More <a href="/about">about me</a> and my work.
     </p>
 
-    <div class="license">
-    <a href="https://liberapay.com/icyphox/donate">
-    <img alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg">
-    </a>
-    <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
+    <div class="icons">
+    <a href="https://creativecommons.org/licensjes/by-nc-sa/4.0/">
     <img class="footimgs" src="/static/cc.svg">
     </a>
     <a href="https://webring.xxiivv.com/#random" target="_blank">
     <img class="footimgs" alt="xxiivv webring" src="/static/webring.svg">
     </a>
     </div>
+
         """
 template = 'text.html'  # default is index.html
-pre_build = [['./bin/openring.py', '-j'], './bin/update_index.py']
-post_build = ['./bin/rss.py', './bin/plaintext.sh']
+pre_build = [['bin/openring.py', '-j'], 'bin/update_index.py']
+post_build = ['bin/rss.py', 'bin/plaintext.sh', 'bin/commit.py']
