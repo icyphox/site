4b8c7819b2f18d04bbd854701b9a883fd26cc169
Anirudh Oppiliappan <x@icyphox.sh>
2020-11-04 11:19:20 +0530

    Remove junk from build script

--- netlify-build.sh
+++ netlify-build.sh
@@ -4,6 +4,4 @@ rm -rf go-vite
 git clone https://github.com/icyphox/go-vite
 cd go-vite && make && cd ..
 mkdir build
-pip install feedparser arrow python-frontmatter
-pip install git+https://github.com/icyphox/myrkdown
 go-vite/vite build
