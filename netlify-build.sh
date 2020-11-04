2a92a00e5d826d1c936ac086ed29d9d7b7b120ba
Anirudh Oppiliappan <x@icyphox.sh>
2020-11-04 11:16:14 +0530

    Add build command

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 git clone https://github.com/icyphox/go-vite
+cd go-vite && make && cd ..
 mkdir build
 pip install feedparser arrow python-frontmatter
 pip install git+https://github.com/icyphox/myrkdown
