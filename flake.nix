c929980c206b627f00afe0bbecc0e1980f5cfd81
Anirudh Oppiliappan <x@icyphox.sh>
2023-03-19 15:20:45 +0200

    nix: add vite to devShell

--- flake.nix
+++ flake.nix
@@ -1,18 +1,28 @@
 {
   description = "site";
 
-  outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux =
-      let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in
-      pkgs.mkShell {
-        buildInputs = with pkgs; [
-          go
-          gotools
-          gnumake
-          entr
-        ];
-      };
+  inputs = {
+    vite.url = "github:icyphox/go-vite";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils, vite }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = [
+              vite.packages.${system}.default
+              pkgs.go
+              pkgs.gotools
+              pkgs.gnumake
+              pkgs.entr
+            ];
+          };
+        }
+      );
+
 }
