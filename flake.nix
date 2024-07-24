bd3d166d51373d07c939c64fa8d9d9b599a15df9
Anirudh Oppiliappan <x@icyphox.sh>
2024-07-24 19:14:21 +0300

    nix: build and deploy via flake apps

--- flake.nix
+++ flake.nix
@@ -1,19 +1,33 @@
 {
   description = "site";
 
-  inputs = {
-    vite.url = "github:icyphox/go-vite";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.vite.url = "github:icyphox/go-vite";
 
-  outputs = { self, nixpkgs, flake-utils, vite }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      vite,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      devShells = forAllSystems (
+        system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = nixpkgsFor.${system};
         in
         {
-          devShell = pkgs.mkShell {
+          default = pkgs.mkShell {
             buildInputs = [
               vite.packages.${system}.vite
               pkgs.gotools
@@ -25,4 +39,28 @@
         }
       );
 
+      apps = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = {
+            type = "app";
+            program = "${pkgs.writeShellScriptBin "vite-build" ''
+              #!/usr/bin/env bash
+              ${vite.packages.${system}.vite}/bin/vite build
+            ''}/bin/vite-build";
+            cwd = ./.;
+          };
+          deploy = {
+            type = "app";
+            program = "${pkgs.writeShellScriptBin "s3-sync" ''
+                #!/usr/bin/env bash
+                ${pkgs.awscli2}/bin/aws s3 sync build s3://site/ --size-only
+            ''}/bin/s3-sync";
+          };
+        }
+      );
+    };
 }
