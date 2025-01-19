39c2f91c086dd56a4f5b1bf5931bb0bc261e29df
Anirudh Oppiliappan <x@icyphox.sh>
2025-01-19 21:46:44 +0200

    bin/build.sh: check if VITE_ENV is production

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+
 rm -rf go-vite
 git clone https://github.com/icyphox/go-vite
 cd go-vite && make && cd ..
 mkdir build
-go-vite/vite build --drafts
+
+if [ "$VITE_ENV" = "production" ]; then
+  go-vite/vite build
+else
+  go-vite/vite build --drafts
+fi
